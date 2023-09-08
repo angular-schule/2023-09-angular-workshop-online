b7e75d6f629e6e1d86d8e10388b105d4ed889652
Ferdinand Malcher <ferdinand@malcher.media>
2023-09-08 10:19:01 +0200

    Stub/Mock für BookRatingService

--- book-rating/src/app/books/dashboard/dashboard.component.spec.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.spec.ts
@@ -1,16 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
+import { Book } from '../shared/book';
+import { BookRatingService } from '../shared/book-rating.service';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
 
   beforeEach(() => {
+
+    // Stub für BookRatingService
+    const ratingMock = {
+      rateUp: (b: Book) => b,
+      rateDown: (b: Book) => b,
+    }
+
     TestBed.configureTestingModule({
       imports: [DashboardComponent],
-      declarations: [],
-      providers: []
+      providers: [
+        // BRS ersetzen: Wenn Service angefordert wird,
+        // wird stattdessen ratingMock ausgeliefert
+        { provide: BookRatingService, useValue: ratingMock }
+      ]
     });
     fixture = TestBed.createComponent(DashboardComponent);
     // TS-Klasseninstanz
