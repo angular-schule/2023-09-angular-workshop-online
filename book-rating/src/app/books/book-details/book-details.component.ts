f08ec6e7df1ab2c25decdff5afb9abdec81dbef2
Ferdinand Malcher <ferdinand@malcher.media>
2023-09-11 11:43:31 +0200

    Routing: Links und Detailseite

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Book } from '../shared/book';
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'app-book-details',
@@ -11,4 +13,21 @@ import { RouterLink } from '@angular/router';
 })
 export class BookDetailsComponent {
 
+  book?: Book;
+
+  constructor(private route: ActivatedRoute, private bs: BookStoreService) {
+    // PULL / synchron
+    // const isbn = this.route.snapshot.paramMap.get('isbn'); // path: 'books/:isbn'
+    // console.log(isbn);
+
+    // PUSH
+    // TODO: Verschachtelte Subscription vermeiden
+    this.route.paramMap.subscribe(params => {
+      const isbn = params.get('isbn')!; // Non-Null Assertion
+
+      this.bs.getSingle(isbn).subscribe(book => {
+        this.book = book;
+      });
+    });
+  }
 }
