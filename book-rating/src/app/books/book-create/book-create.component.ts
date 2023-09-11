99b399de6081ef021b913cb02c26739b6193d8c9
Ferdinand Malcher <ferdinand@malcher.media>
2023-09-11 14:24:44 +0200

    Reactive Forms: Modell und HTML

--- book-rating/src/app/books/book-create/book-create.component.ts
+++ book-rating/src/app/books/book-create/book-create.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-book-create',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './book-create.component.html',
   styleUrls: ['./book-create.component.scss']
 })
 export class BookCreateComponent {
-
+  bookForm = new FormGroup({
+    isbn: new FormControl('', { nonNullable: true }),
+    title: new FormControl('', { nonNullable: true }),
+    description: new FormControl('', { nonNullable: true }),
+    rating: new FormControl(3, { nonNullable: true }),
+    price: new FormControl(0, { nonNullable: true }),
+  });
 }
