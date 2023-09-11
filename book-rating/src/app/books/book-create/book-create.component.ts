b1aa71e2d72cc210446f88f12ee179353aa90296
Ferdinand Malcher <ferdinand@malcher.media>
2023-09-11 15:27:20 +0200

    Forms: Validierung

--- book-rating/src/app/books/book-create/book-create.component.ts
+++ book-rating/src/app/books/book-create/book-create.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-book-create',
@@ -11,10 +11,48 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 })
 export class BookCreateComponent {
   bookForm = new FormGroup({
-    isbn: new FormControl('', { nonNullable: true }),
-    title: new FormControl('', { nonNullable: true }),
+    isbn: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.pattern(/^[0-9]*$/),
+        Validators.minLength(10),
+        Validators.maxLength(13)
+      ]
+    }),
+    title: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.maxLength(80)]
+    }),
     description: new FormControl('', { nonNullable: true }),
-    rating: new FormControl(3, { nonNullable: true }),
-    price: new FormControl(0, { nonNullable: true }),
+    rating: new FormControl(3, {
+      nonNullable: true,
+      validators: [
+        Validators.min(1),
+        Validators.max(5)
+      ]
+    }),
+    price: new FormControl(0, {
+      nonNullable: true,
+      validators: [Validators.required, Validators.min(0)]
+    }),
   });
 }
+
+
+
+/*
+TODO:
+- Fehlermeldungen anzeigen
+  - "ISBN ist ungültig"
+  - "ISBN ist zu kurz"
+- Submit-Button
+- abschicken
+- Buch erstellen
+- HTTP
+- bei Erfolg:
+  - wegnavigieren, z. B. zum Dashboard oder Detailseite
+  - Meldung anzeigen
+  - zurücksetzen
+
+*/
