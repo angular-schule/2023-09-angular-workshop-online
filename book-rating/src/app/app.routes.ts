8ae3986735f72ab53423363bc3b3e74de0bc34a6
Ferdinand Malcher <ferdinand@malcher.media>
2023-09-11 13:14:04 +0200

    Lazy Loading

--- book-rating/src/app/app.routes.ts
+++ book-rating/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
-import { booksRoutes } from './books/books.routes';
+
+// import { booksRoutes } from './books/books.routes';
 
 export const routes: Routes = [
   // bei Weietrleitung vom leeren Pfad fast immer pathMatchFull nötig
   { path: '', redirectTo: 'books', pathMatch: 'full' },
-  ...booksRoutes
+  // ...booksRoutes
+  {
+    path: 'books',
+    loadChildren: () => import('./books/books.routes').then(m => m.booksRoutes)
+  }
 ];
 
 
--- book-rating/src/app/books/books.routes.ts
+++ book-rating/src/app/books/books.routes.ts
@@ -3,6 +3,6 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
 
 export const booksRoutes: Routes = [
-  { path: 'books', component: DashboardComponent },
-  { path: 'books/:isbn', component: BookDetailsComponent }
+  { path: '', component: DashboardComponent },
+  { path: ':isbn', component: BookDetailsComponent }
 ];
